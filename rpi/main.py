498a9dfc0f68bd92dea2983f78a086d80f38b8cf
aaronyong <yongaaron11@gmail.com>
2023-07-16 01:51:16 +0800

    rpi main todo

--- rpi/main.py
+++ rpi/main.py
@@ -1,2 +1,15 @@
 import movement as m
 
+# start bluetooth server
+# receive arena info (BT)
+
+# generate algo solution
+
+# execute algo path
+# LOOP
+# move 
+# ping back location (BT)
+# if SNAP: detect image (TCP) 
+# ping back image_id (BT)
+# update image_count
+# if image_count >= 5: break
